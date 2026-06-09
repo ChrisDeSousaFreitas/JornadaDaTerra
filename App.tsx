ed5f51deb2780fbbc319866007253c6d80c82cd9
chrisdesousafreitas <chrissf0211@gmail.com>
2026-06-09 16:43:43 -0300

    feat: construcao do roteamento e interface base HUD (Home e Navbar)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './src/types';
 
-// Importação das Telas (sem as notificações)
 import HomeScreen from './src/screens/HomeScreen';
 import QuestsScreen from './src/screens/QuestsScreen';
 import AddQuestScreen from './src/screens/AddQuestScreen';
@@ -17,20 +17,23 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#0B1221" />
       <Stack.Navigator
         screenOptions={{
-          headerStyle: { backgroundColor: '#0B132B' },
-          headerTintColor: '#E9C46A',
-          headerTitleStyle: { fontWeight: 'bold' },
+          headerStyle: { backgroundColor: '#0B1221' },
+          headerTintColor: '#00E5FF',
+          headerTitleStyle: { fontWeight: '900' },
+          headerShadowVisible: false,
+          contentStyle: { backgroundColor: '#0B1221' }
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Quests" component={QuestsScreen} options={{ title: 'Monitoramento' }} />
-        <Stack.Screen name="AddQuest" component={AddQuestScreen} options={{ title: 'Nova Ocorrência' }} />
+        <Stack.Screen name="Quests" component={QuestsScreen} options={{ title: 'Painel de Missões' }} />
+        <Stack.Screen name="AddQuest" component={AddQuestScreen} options={{ title: 'Reportar Anomalia' }} />
         <Stack.Screen name="QuestDetail" component={QuestDetailScreen} options={{ title: 'Detalhes do Alerta' }} />
-        <Stack.Screen name="EditQuest" component={EditQuestScreen} options={{ title: 'Ajustar Medidas' }} />
-        <Stack.Screen name="Stats" component={StatsScreen} options={{ title: 'Estatísticas' }} />
-        <Stack.Screen name="Map" component={MapScreen} options={{ title: 'Mapa Orbital' }} />
+        <Stack.Screen name="EditQuest" component={EditQuestScreen} options={{ title: 'Ajustar Tática' }} />
+        <Stack.Screen name="Stats" component={StatsScreen} options={{ title: 'Telemetria' }} />
+        <Stack.Screen name="Map" component={MapScreen} options={{ title: 'Radar Orbital' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
