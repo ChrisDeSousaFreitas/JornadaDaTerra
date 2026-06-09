bd289482611ffbde4162968f5bba1d99f9b8d975
chrisdesousafreitas <chrissf0211@gmail.com>
2026-06-09 14:19:39 -0300

    feat: logica de persistencia offline com asyncstorage

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,9 @@
-import axios from 'axios';
-const api = axios.create({ baseURL: 'http://SEU_IP:3000' });
-export default api;
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const saveOffline = async (key: string, data: any) => {
+  await AsyncStorage.setItem(key, JSON.stringify(data));
+};
+export const getOffline = async (key: string) => {
+  const data = await AsyncStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+};
